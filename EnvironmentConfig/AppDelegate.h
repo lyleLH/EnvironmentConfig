817fc7e855317a6f991a9cab146f03458cbbd48c
lyleKP <v2top1@163.com>
2016-07-04 11:33:12 +0800

    Initial Commit

---
+++ EnvironmentConfig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnvironmentConfig
+//
+//  Created by lyleKP on 16/7/4.
+//  Copyright © 2016年 lyleKP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnvironmentConfig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnvironmentConfig
+//
+//  Created by lyleKP on 16/7/4.
+//  Copyright © 2016年 lyleKP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
